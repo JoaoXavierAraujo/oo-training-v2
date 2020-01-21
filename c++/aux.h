04738dbd4c263051fb38bfc2be09066611e8bb93
root <root@CTW-00782.criticaltechworks.com>
2020-01-21 11:34:49 +0000

    added bazel build

---
+++ c++/aux.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <exception>
+#include <string>
+#include <stdexcept>
+#include <vector>
+#include <cmath>
+#include "exerc/exceptional_server.h"
+
+using namespace std;
+
+void run_exceptional_server();
